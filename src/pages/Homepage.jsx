13f14a88c85b30cb7f15445864e85b8fbf94807b
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2023-12-30 10:35:33 +0200

    Create basic pages and html arhitecture

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom';
 import styles from './Homepage.module.css';
+import PageNav from '../components/PageNav';
 export default function Homepage() {
   return (
     <main className={styles.homepage}>
+      <PageNav/>
     <section>
       <h1>
         You travel the world.
@@ -13,6 +16,7 @@ export default function Homepage() {
         of. Never forget your wonderful experiences, and show your friends how
         you have wandered the world.
       </h2>
+      <Link to="/app" className='cta'>Start tracking now</Link>
     </section>
   </main>
   );
