cd6afb40d85018cf1b53ff790f3ca9c940691a11
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2023-12-30 11:30:31 +0200

    Create static layout and nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,17 @@ function App() {
   return (
      <BrowserRouter>
        <Routes>
+         <Route index element={<Homepage/>}/>
          <Route path="product" element={<Product/>}/>
          <Route path="login" element={<Login/>}/>
-         <Route path="/" element={<Homepage/>}/>
          <Route path="pracing" element={<Pricing/>}/>
-         <Route path="app" element={<AppLayout/>}/>
+         <Route path="app" element={<AppLayout/>}>
+          {/* If case no one of child routes are not executed */}
+           <Route index element={<p>List of cities</p>}/>
+           <Route path="cities" element={<p>List of cities</p>}/>
+           <Route path="countries" element={<p>List of countries</p>}/>
+           <Route path="form" element={<p>form</p>}/>
+         </Route>
          <Route path="*" element={<PageNotFount/>}/>
        </Routes>
      </BrowserRouter>
